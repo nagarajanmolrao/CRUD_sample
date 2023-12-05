9731d61f3476f7e269af5493af8e3324d9c37368
n-anmol <n.anmol@capgemini.com>
2023-12-05 16:24:48 +0530

    initial commit

---
+++ backend/src/main/java/com/crud/sample/model/Task.java
@@ -0,0 +1,26 @@
+package com.crud.sample.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+}
